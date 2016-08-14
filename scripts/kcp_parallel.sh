9267c8f225594871ad3bdc7fdb405017268275c0
Alex Clark <alexsclark@gmail.com>
2016-08-14 16:09:44 +0100

    alex

--- scripts/kcp_parallel.sh
+++ scripts/kcp_parallel.sh
@@ -1,5 +1,5 @@
 cd ../testcfg/
 for slice in `seq 1 40`
 do
-    python run_experiment_cnf_kfcp_parallel.py --seed $slice --grammars 40 ../data/kfcp.$slice &
+    python run_experiment_cnf_kfcp_parallel.py --seed $slice --grammars 25 ../data/kfcp.$slice > ../log/kfcp.log.$slice &
 done
